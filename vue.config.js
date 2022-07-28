c5a84943f54958230f0010a73eff924a44abfda3
Counting-China-Women-Football <Women_Football@163.com>
2022-07-28 09:31:54 +0800

    copy

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+
+const path = require('path')
+module.exports = {
+  publicPath: '/Chinese-Women-Football-Team/',
+  outputDir: 'dist',
+  transpileDependencies: true,
+  devServer: {
+    // 调试端口
+    port: 8989
+  },
+
+}
